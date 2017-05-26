25069389d69b1fa44e50e2d91975d4a1ad06e03f
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-05-26 02:56:34 -0400

    Updated reference to SystemExtensions

--- src/GameAI/MonteCarlo.cs
+++ src/GameAI/MonteCarlo.cs
@@ -1,6 +1,6 @@
-﻿using CSharpExtras.Random;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using SystemExtensions.Random;
 
 namespace GameAI
 {
--- src/GameAI/MonteCarloTree.cs
+++ src/GameAI/MonteCarloTree.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using CSharpExtras.Random;
+using SystemExtensions.Random;
 
 namespace GameAI
 {
