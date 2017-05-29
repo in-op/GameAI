f82718af60fded55f1bff7a8bb053b8d5d83ad7b
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-05-28 20:45:17 -0400

    Changed project XML file to revert back to 4.7. Began creating parallel method

--- src/GameAI/MonteCarlo/RandomSimulation.cs
+++ src/GameAI/MonteCarlo/RandomSimulation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using SystemExtensions;
 using SystemExtensions.Random;
 
 namespace GameAI.MonteCarlo
@@ -45,6 +46,50 @@ namespace GameAI.MonteCarlo
 
 
 
+        public static TMove Search<TMove>(IGame<TMove> game, int simulations, bool useParallel)
+        {
+            if (useParallel)
+            {
+
+            }
+            else
+            {
+                return Search(game, simulations);
+            }
+        }
+
+
+        private class LocalLoopVariables<TMove>
+        {
+            public int aiPlayer;
+            public List<TMove> legalMoves;
+            public int count;
+            public MoveStats[] moveStats;
+            public int moveIndex;
+            public IGame<TMove> copy;
+            public Random rng;
+
+            public LocalLoopVariables(int aiPlayer, List<TMove> legalMoves, int count, MoveStats[] moveStats, int moveIndex, IGame<TMove> copy, Random rng)
+            {
+                this.aiPlayer = aiPlayer;
+                this.legalMoves = legalMoves;
+                this.count = count;
+                this.moveStats = moveStats;
+                this.moveIndex = moveIndex;
+                this.copy = copy;
+                this.rng = rng;
+            }
+        }
+
+        public static TMove ParallelSearch<TMove>(IGame<TMove> game, int simulations)
+        {
+            List<TMove> legalMoves = game.GetLegalMoves();
+
+
+        }
+
+
+
         /// <summary>
         /// Returns the move found to have highest win-rate
         /// after performing the specified number of
@@ -53,14 +98,13 @@ namespace GameAI.MonteCarlo
         /// <param name="game">The current game.</param>
         /// <param name="simulations">The number of simulations to perform.</param>
         /// <typeparam name="TMove">The type of the moves in the IGame implementation.</typeparam>
-        public static TMove Search<TMove>(IGame<TMove> game, int simulations)
+            public static TMove Search<TMove>(IGame<TMove> game, int simulations)
         {
             // hoist all declarations out of the main loop for performance
             int aiPlayer = game.GetCurrentPlayer();
             List<TMove> legalMoves = game.GetLegalMoves();
             int count = legalMoves.Count;
-            MoveStats[] moveStats = new MoveStats[count];
-            for (int i = 0; i < count; i++) moveStats[i] = new MoveStats();
+            MoveStats[] moveStats = JaggedArray.Create(count, new MoveStats());
             int moveIndex;
             IGame<TMove> copy;
             Random rng = new Random();
