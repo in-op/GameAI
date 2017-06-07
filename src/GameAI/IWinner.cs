95fb1ffa7436c43bb103e28b1ccadaf3b50179b1
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-06-06 23:58:01 -0400

    Updated doc comments for IWinner

--- src/GameAI/IWinner.cs
+++ src/GameAI/IWinner.cs
@@ -2,6 +2,11 @@
 {
     public interface IWinner<TPlayer>
     {
+        /// <summary>
+        /// Returns whether the given player is victories in the current gamestate.
+        /// </summary>
+        /// <param name="player">The player for whom to check victory.</param>
+        /// <returns>True if the player is victorious in the current gamestate, false otherwise.</returns>
         bool IsWinner(TPlayer player);
     }
 }
